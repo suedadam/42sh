1183571e46d981ec062a676a0fd96b44ea9766be
nkouris <nkouris@student.42.us.org>
2018-04-05 13:43:17 -0700

    cuts and yanks hooked up

--- src/escape_sequences/ft_alt.c
+++ src/escape_sequences/ft_alt.c
@@ -6,19 +6,14 @@
 /*   By: nkouris <nkouris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/04 18:15:12 by nkouris           #+#    #+#             */
-/*   Updated: 2018/04/05 11:38:08 by nkouris          ###   ########.fr       */
+/*   Updated: 2018/04/05 13:42:47 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_term.h"
 
-int		ft_alt(char byte)
+static int	ft_jumps(char *temp)
 {
-	char	temp[2];
-
-	T_DBLESC = 0;
-	byte = 0;
-	read(STDIN_FILENO, temp, 2);
 	if (temp[1] == 'A')
 		jump_prev_line(&g_shell_env.cursor);
 	else if (temp[1] == 'B')
@@ -30,4 +25,35 @@ int		ft_alt(char byte)
 	else
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
+}
+
+static int	ft_edit(char byte)
+{
+	if (byte == 'w')
+		cut_word_before_cursor();
+	else if (byte == 'k')
+		cut_line_after_cursor();
+	else if (byte == 'u')
+		cut_line_before_cursor();
+	else if (byte == 'y')
+		ft_printf(" OH YEAH");
+	else
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+int		ft_alt(char byte)
+{
+	char	temp[2];
+	int		ret;
+
+	T_DBLESC = 0;
+	if (byte == '[')
+	{
+		read(STDIN_FILENO, temp, 2);
+		ret = ft_jumps(temp);
+	}
+	else
+		ret = ft_edit(byte);
+	return (ret);
 }
