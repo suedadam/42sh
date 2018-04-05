7195ed0cbfbdbb227bfb155620908aff09f011a8
Terence Le Huu Phuong <tle-huu-@e1z2r2p15.42.us.org>
2018-04-04 21:07:25 -0700

    cursor navigation fixed, next to test : cut and copy pasting

--- src/cursor_navigation.c
+++ src/cursor_navigation.c
@@ -6,7 +6,7 @@
 /*   By: tle-huu- <tle-huu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/04 15:18:41 by tle-huu-          #+#    #+#             */
-/*   Updated: 2018/04/04 19:37:59 by tle-huu-         ###   ########.fr       */
+/*   Updated: 2018/04/04 21:06:11 by tle-huu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,31 @@
 
 void			move_prev_word(t_cursor *cursor)
 {
-	if (cursor->position && ft_isalnum(cursor->buffer[cursor->position]))
-	{
-		while (cursor->position && cursor->buffer[cursor->position] != ' ')
-			cursor_to_left(cursor);
-		if (cursor->position)
-			cursor_to_right(cursor);
-	}
-	else if (cursor->position)
-	{
-		while (cursor->position && cursor->buffer[cursor->position] == ' ')
-			cursor_to_left(cursor);
-		move_prev_word(cursor);
-	}
+	if ((cursor->position && (cursor->buffer[cursor->position]) != ' '
+		&& cursor->buffer[cursor->position - 1] == ' ')
+			|| cursor->position == g_shell_env.buffer->length)
+		cursor_to_left(cursor);
+	while (cursor->position && cursor->buffer[cursor->position] == ' ')
+		cursor_to_left(cursor);
+	while (cursor->position && (cursor->buffer[cursor->position]) != ' ')
+		cursor_to_left(cursor);
+	if (cursor->position)
+		cursor_to_right(cursor);
 }
 
 void			move_next_word(t_cursor *cursor)
 {
-	if (cursor->position != g_shell_env.buffer->length && ft_isalnum(cursor->buffer[cursor->position]))
-	{
-		while (cursor->buffer[cursor->position] != ' ')
-			cursor_to_right(cursor);
-		move_next_word(cursor);
-	}
-	else if (cursor->position != g_shell_env.buffer->length)
-		while (cursor->position != g_shell_env.buffer->length
-				&& cursor->buffer[cursor->position] == ' ')
-			cursor_to_right(cursor);
+	while ((cursor->buffer[cursor->position]) != ' ' && cursor->position !=
+			g_shell_env.buffer->length)
+				cursor_to_right(cursor);
+	while (cursor->buffer[cursor->position] == ' ' && cursor->position !=
+			g_shell_env.buffer->length)
+				cursor_to_right(cursor);
 }
 
 void			move_next_line(t_cursor *cursor)
 {
-	if (cursor->position + g_shell_env.window.ws_col >
-			g_shell_env.buffer->length)
+	if ((cursor->position + g_shell_env.window.ws_col) < g_shell_env.buffer->length)
 		cursor->position += g_shell_env.window.ws_col;
 	else
 		cursor->position = g_shell_env.buffer->length;
@@ -55,7 +47,7 @@ void			move_next_line(t_cursor *cursor)
 
 void			move_prev_line(t_cursor *cursor)
 {
-	if (cursor->position + cursor->og_position.x < g_shell_env.window.ws_col)
+	if (cursor->position < g_shell_env.window.ws_col)
 		cursor->position = 0;
 	else
 		cursor->position -= g_shell_env.window.ws_col;
