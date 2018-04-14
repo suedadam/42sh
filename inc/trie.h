be1f668fac62db5db54c80ec458814603896722e
Terence Le Huu Phuong <tle-huu-@e1z1r8p6.42.us.org>
2018-04-13 18:07:07 -0700

    trie.h

--- inc/trie.h
+++ inc/trie.h
@@ -6,7 +6,7 @@
 /*   By: tle-huu- <tle-huu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/12 19:43:15 by tle-huu-          #+#    #+#             */
-/*   Updated: 2018/04/12 20:54:16 by tle-huu-         ###   ########.fr       */
+/*   Updated: 2018/04/13 18:04:27 by tle-huu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 typedef struct s_trie
 {
 	struct s_trie	*children[129];
+	char			key;
 	int				nbr_children;
 }				t_trie;
 
@@ -31,3 +32,52 @@ void		display_trie(t_trie *trie, int level);
 
 
 #endif
+
+{
+	t_list		*gothrough;
+	t_list		*node_list;
+	t_node		*node;
+
+	gothrough = parent->neighbors;
+	while (gothrough)
+	{
+		node = graph->tab[((t_nghbr_node *)(gothrough->content))->index];
+		if (node->dist < 0)
+		{
+			node->dist = dist + 1;
+			node->parent = parent->index;
+			node_list = ft_lstnew(node, sizeof(*node));
+			enqueue(queue, node_list);
+		}
+		gothrough = gothrough->next;
+	}
+}
+
+int				bfs(t_graph *graph, int start)
+{
+	t_queue		*queue;
+	t_list		*tmp;
+	t_node		*node;
+	int			dist;
+
+	queue = new_queue();
+	dist = 0;
+	graph->tab[start]->dist = dist;
+	tmp = ft_lstnew(graph->tab[start], sizeof(*(graph->tab[start])));
+	enqueue(queue, tmp);
+	while (!queue_empty(queue))
+	{
+		tmp = queue_pop(queue);
+		node = (t_node *)(tmp->content);
+		gothrough_neighbors(graph, node, node->dist, queue);
+		free(tmp->content);
+		free(tmp);
+	}
+	free(queue);
+	if (graph->tab[graph->start]->parent < 0)
+	{
+		ft_printf("\nBFS error\n");
+		return (ERROR);
+	}
+	return (SUCCESS);
+}
--- src/termio/autocomplete/autocomplete.c
+++ src/termio/autocomplete/autocomplete.c
@@ -6,11 +6,13 @@
 /*   By: tle-huu- <tle-huu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/12 18:02:51 by tle-huu-          #+#    #+#             */
-/*   Updated: 2018/04/13 16:13:40 by tle-huu-         ###   ########.fr       */
+/*   Updated: 2018/04/13 18:02:19 by tle-huu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "autocomplete.h"
+
+#define NO_CHILD -42
 
 /*
 ** Might be static in the future
@@ -29,12 +31,49 @@ static int		is_rope(t_trie *trie)
 
 static void		push_children(t_trie *trie, t_list *stack)
 {
+	int		i;
+
+	i = 127;
+	while (i >= 0)
+	{
+		if (trie->array[i])
+			ft_stackpush(stack, trie->array[i], sizeof(trie->array[i]));
+		i--;
+	}
+}
+
+static int		fill_buffer_with_rope(t_trie *trie)
+{
 
 }
 
-int				autocomplete_trie_dfs(t_trie *trie)
+static int		rope_child_index(t_trie *trie)
 {
-	static t_list		*stack = NULL;
+	int		i;
 
+	i = 0;
+	if (trie->nbr_children <= 0)
+		return (NO_CHILD);
+	while (trie->array[i])
+	{
+		if (trie->array[i])
+			return (i);
+		i++;
+	}
+	return (NO_CHILD);
+}
+
+void				autocomplete_trie_dfs(t_trie *trie)
+{
+	static t_stack		*stack = new_stack();
+	static int			length = 0;
+	int					distance;
+	t_trie				*head;
+
+	distance = 0;
+	while (!isempty_stack(stack))
+	{
+
+	}
 
 }
