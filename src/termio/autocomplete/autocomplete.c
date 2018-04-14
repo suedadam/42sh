95625c0ae2f0442f87131b2d478329222f9ae582
Terence Le Huu Phuong <tle-huu-@e1z1r8p6.42.us.org>
2018-04-13 22:09:59 -0700

    autocomplete dfs

--- src/termio/autocomplete/autocomplete.c
+++ src/termio/autocomplete/autocomplete.c
@@ -6,55 +6,22 @@
 /*   By: tle-huu- <tle-huu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/12 18:02:51 by tle-huu-          #+#    #+#             */
-/*   Updated: 2018/04/13 18:02:19 by tle-huu-         ###   ########.fr       */
+/*   Updated: 2018/04/13 22:03:02 by tle-huu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "autocomplete.h"
+#include "ft_autocomplete.h"
 
 #define NO_CHILD -42
 
-/*
-** Might be static in the future
-*/
-t_list		*g_trie_stack = NULL;
-
-static int		is_bottleneck(t_trie *trie)
-{
-	return (!!trie->nbr_children);
-}
-
-static int		is_rope(t_trie *trie)
-{
-	return (trie->nbr_children == 1);
-}
-
-static void		push_children(t_trie *trie, t_list *stack)
+static int		next_child(t_trie *trie, int start)
 {
 	int		i;
 
-	i = 127;
-	while (i >= 0)
-	{
-		if (trie->array[i])
-			ft_stackpush(stack, trie->array[i], sizeof(trie->array[i]));
-		i--;
-	}
-}
-
-static int		fill_buffer_with_rope(t_trie *trie)
-{
-
-}
-
-static int		rope_child_index(t_trie *trie)
-{
-	int		i;
-
-	i = 0;
-	if (trie->nbr_children <= 0)
+	i = start;
+	if (trie->nbr_c)
 		return (NO_CHILD);
-	while (trie->array[i])
+	while (i < 128)
 	{
 		if (trie->array[i])
 			return (i);
@@ -63,17 +30,68 @@ static int		rope_child_index(t_trie *trie)
 	return (NO_CHILD);
 }
 
-void				autocomplete_trie_dfs(t_trie *trie)
+static void			no_child(t_trie *trie, t_stack *stack)
+{
+	return ;
+}
+
+static void	one_child(t_trie *trie)
+{
+	int		child;
+
+	child = next_child(trie, 0);
+	regular_text((char)child);
+	trie_dfs(trie->array[child]);
+}
+
+static void	bottleneck(t_trie *trie, t_stack **stack)
+{
+	int			child;
+	t_cursor	*cursor;
+
+	if (isempty_stack(stack) || stack->top->content != trie)
+	{
+		regular_text(trie->key);
+		ft_stackpush(stack, trie, sizeof(t_trie_with_level));
+		cursor = g_shell_env.cursor;
+		((t_trie_with_level *)(trie))->pos = cursor->position;
+		((t_trie_with_level *)(trie))->char = 0;
+		trie_dfs(trie);
+	}
+	else if ((child = next_child(trie, (int)(((t_trie_with_level *)(trie))->child))) != NO_CHILD)
+	{
+		((t_trie_with_level *)(trie))->child = child;
+		trie_dfs(trie->array[child]);
+	}
+	else if (child == NO_CHILD)
+	{
+		free(ft_stackpop(*stack));
+		trie_dfs((t_trie *)((*stack)->top->content));
+	}
+}
+
+static void			resume_dfs(t_trie *trie)
+{
+	t_cursor			*cursor;
+
+	cursor = g_shell.cursor;
+	while (cursor->position != ((t_trie_with_level)(trie))->pos)
+		ft_delete(-1);
+	trie_dfs(parent);
+}
+
+void				trie_dfs(t_trie *trie)
 {
 	static t_stack		*stack = new_stack();
-	static int			length = 0;
-	int					distance;
-	t_trie				*head;
 
-	distance = 0;
-	while (!isempty_stack(stack))
-	{
-
-	}
-
+	if (!trie && isempty_stack(stack))
+		return ;
+	else if (!trie)
+		resume_dfs((t_trie *)(stack->top->content));
+	if (trie->nbr_children == 1)
+		one_child(trie);
+	else if (trie->nbr_children > 1)
+		bottleneck(trie);
+	else if (trie->nbr_children == 0)
+		no_child(trie);
 }
