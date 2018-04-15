346145206d181c1c8c535d4d8cfdf97a75b26352
suedadam <suedadam@gmail.com>
2018-04-14 20:18:16 -0700

    Fix compile issue on fg

--- src/execution/builtins/fg.c
+++ src/execution/builtins/fg.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/14 20:05:58 by asyed             #+#    #+#             */
-/*   Updated: 2018/04/14 20:14:19 by asyed            ###   ########.fr       */
+/*   Updated: 2018/04/14 20:15:54 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,5 @@ int	builtin_fg(char *argv[], __attribute__((unused)) t_environ *env)
 	if (!argv || !argv[0])
 		return (EXIT_FAILURE);
 	unsuspend(argv[1]);
+	return (EXIT_SUCCESS);
 }
