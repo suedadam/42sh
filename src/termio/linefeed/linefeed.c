bcf33f398300cdb3a705e300fe1356276e605942
nkouris <nkouris@student.42.us.org>
2018-04-15 11:20:59 -0700

    history wraps correctly

--- src/termio/linefeed/linefeed.c
+++ src/termio/linefeed/linefeed.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/05 11:52:17 by nkouris           #+#    #+#             */
-/*   Updated: 2018/04/14 22:40:05 by asyed            ###   ########.fr       */
+/*   Updated: 2018/04/15 11:18:17 by nkouris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int			ft_linefeed(void)
 
 	if (g_shell_env.buffer->length)
 		history_append_file(g_shell_env.buffer->buff);
+	g_shell_env.history_var.current_history_cmd = NULL;
 	ft_restoretty();
 	signal(SIGINT, SIG_IGN);
 	// signal(SIGTSTP, SIG_IGN);
