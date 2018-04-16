059840880535b3ea276e69f0277a7a4b73af8a34
Terence Le Huu Phuong <tle-huu-@e1z1r8p6.42.us.org>
2018-04-16 04:11:36 -0700

    Fix merge

--- src/history/history_utils.c
+++ src/history/history_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   history_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nkouris <nkouris@student.42.us.org>        +#+  +:+       +#+        */
+/*   By: nkouris <nkouris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/16 02:37:57 by nkouris           #+#    #+#             */
-/*   Updated: 2018/04/16 03:20:38 by nkouris          ###   ########.fr       */
+/*   Updated: 2018/04/16 04:10:37 by tle-huu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,13 @@ char			*create_home_assoc(char *filename)
 {
 	char	*homepath;
 	char	*temp;
+	char	*freer;
 
-	temp = __getenv("HOME", g_environ);
+	temp = ft_getenv("HOME", g_environ);
 	homepath = concatpath("/", temp);
+	freer = homepath;
 	homepath = concatpath(filename, homepath);
+	meta_free(freer);
 	return (homepath);
 }
 
@@ -48,12 +51,13 @@ int				history_init(void)
 	t_hist_var	*history_var;
 
 	history_var = &g_shell_env.hist_var;
-	if (!(history_var->history_list = new_dblist()))
-	{
-		printf("\ndblist fail\n");
-		return (EXIT_FAILURE);
-	}
+	// if (!(history_var->history_list = new_dblist()))
+	// {
+	// 	printf("\ndblist fail\n");
+	// 	return (EXIT_FAILURE);
+	// }
 	history_var->current_history_cmd = NULL;
-	history_var->history_list = get_history();
+	if (!(history_var->history_list = get_history()))
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
