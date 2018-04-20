9cba9562bef5f78a1597bfdfcce2c383ddf0cf95
Terence Le Huu Phuong <tlehuu@hotmail.fr>
2018-04-20 16:32:34 -0700

    double quote and quote fixsed

--- src/termio/hanging_byte/hanging_byte.c
+++ src/termio/hanging_byte/hanging_byte.c
@@ -6,7 +6,7 @@
 /*   By: nkouris <nkouris@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 14:15:45 by nkouris           #+#    #+#             */
-/*   Updated: 2018/04/18 23:17:27 by nkouris          ###   ########.fr       */
+/*   Updated: 2018/04/20 16:32:10 by tle-huu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,10 @@ void	verify_hanging(void)
 
 void	hanging_byte(char byte)
 {
-	if (byte == '\'' && (!T_DQUOTE || !T_QUOTE))
-		T_QUOTE = (T_QUOTE == 1) ? 0 : 1;
-	else if (byte == '\"' && (!T_QUOTE || !T_DQUOTE))
-		T_DQUOTE = (T_DQUOTE == 1) ? 0 : 1;
+	if (byte == '\'' && (!T_DQUOTE))
+		T_QUOTE = (T_QUOTE == 1) ? 0 : 1 * !T_DQUOTE;
+	else if (byte == '\"' && (!T_QUOTE))
+		T_DQUOTE = (T_DQUOTE == 1) ? 0 : 1 * !T_QUOTE;
 	else if (byte == '(' || byte == ')')
 		paren_check(byte);
 	else if (byte == '{' || byte == '}')
